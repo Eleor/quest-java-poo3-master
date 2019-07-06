da74cc3cea5483bd5bac9fa9b74a61901727da98
Eleor <52451388+Eleor@users.noreply.github.com>
2019-07-06 16:14:15 +0200

    Add files via upload

---
+++ quest-java-poo3-master/Eagle.java
@@ -0,0 +1,60 @@
+public class Eagle extends Bird implements Fly {
+
+    private boolean flying;
+    private int altitude;
+
+    public Eagle(String name) {
+        super(name);
+        this.flying = false;
+        this.altitude = 0;
+    }
+
+    public int getAltitude() {
+        return altitude;
+    }
+
+    public boolean isFlying() {
+        return flying;
+    }
+
+    @Override
+    public String sing() {
+        return "Screech!";
+    }
+    
+    @Override
+   public void takeOff() {
+    	if (!this.flying && this.altitude == 0) {
+            this.flying = true;
+            System.out.printf("%s take of into the air. %n", this.getName());
+        }	
+    }
+	
+    @Override
+	public int ascend(int meters) {
+    	if (this.flying) {
+            this.altitude = Math.max(this.altitude + meters, 0);
+            System.out.printf("%s flys upward, altitude : %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;	
+    }
+	
+    @Override
+	public int descend(int meters) {
+    	if (this.flying) {
+            this.altitude = Math.min(this.altitude - meters,  325);
+            System.out.printf("%s flys downward, altitude : %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+	
+    @Override
+	public void land() {
+    	if (this.flying && this.altitude == 0) {
+            System.out.printf("%s lands on the earth.%n", this.getName());
+        } else {
+            System.out.printf("%s is too high, it can't land.%n", this.getName());
+        }
+    	
+    }
+}
